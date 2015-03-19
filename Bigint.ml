218dbe3674f250cd06c97f9ae6ba7bea4800b8a2
amstut_a <arthur.amstutz@epitech.eu>
2015-03-19 18:16:07 +0100

    convert base compile pas

--- Bigint.ml
+++ Bigint.ml
@@ -1,10 +1,6 @@
-open Char
-open List
-
 type bigint = {
   value : (char list);
-  sign : int
-}
+  sign : int }
 
 let rec print_list = function
   | h::t ->
@@ -12,9 +8,29 @@ let rec print_list = function
        print_char h;
        print_list t
      end
-  | [] -> ()
+  | []   -> ()
+
+let convert_base from tob nbr =
+  let diviseur = (String.length tob) in
+  let rec cbase_in aff = function
+    | 0 -> aff
+    | _ ->
+       begin
+	 let quotient = nbr / diviseur in
+	 let reste = nbr mod diviseur in
+	 let nbr = quotient in
+	 if reste >= 0 && reste <= 9 then
+	   cbase_in reste::aff quotient
+	 else
+	   cbase_in (reste + 55)::aff quotient
+       end
+  in cbase_in [] 1
+  ;;
+
 
 let a = 'e'::'a'::[];;
 let b = { value = a; sign = 0 };;
   print_list b.value;;
     print_endline "";;
+      
+      convert_base "0123456789" "01" 10;
