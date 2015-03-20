bb79ea0334eb7539e293665e719bc1300e44aa44
amstut_a <arthur.amstutz@gmail.com>
2015-03-20 17:49:51 +0100

    add recuperation ops et nombres dans une expression

--- eval_expr.ml
+++ eval_expr.ml
@@ -1,3 +1,7 @@
+type bigint = {
+  value : (char list);
+  sign : int }
+
 type expression =
   | Sum of expression * expression
   | Sub of expression * expression
@@ -13,16 +17,57 @@ let rec eval_expr = function
   | Div(expr1, expr2) -> (eval_expr expr1) / (eval_expr expr2)
   | Mod(expr1, expr2) -> (eval_expr expr1) mod (eval_expr expr2)
   | Val(value) -> value;;
+ 
 
-let rec compile_expr = function
-  | Sum(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "+"; (compile_expr expr2); ")"]
-  | Sub(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "-"; (compile_expr expr2); ")"]
-  | Mul(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "*"; (compile_expr expr2); ")"]
-  | Div(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "/"; (compile_expr expr2); ")"]
-  | Mod(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "%"; (compile_expr expr2); ")"]
-  | Val(value) -> string_of_int value
+let rec getops str idx l =
+  let len = (String.length str) in
+  if idx = len then (List.rev l)
+  else match str.[idx] with
+       | '+' | '-' | '*' | '/' | '%' -> getops str (idx + 1) (str.[idx]::l)
+       | _ -> getops str (idx + 1) l
 
+
+let rec get_cur_nbr str idx =
+  let len = (String.length str) in
+  if idx >= len then []
+  else match str.[idx] with
+       | '+' | '-' | '*' | '/' | '%' | '(' | ')' | ' ' -> []
+       | _ -> str.[idx]::(get_cur_nbr str (idx + 1))
+
+let rec get_next_op str idx =
+  let len = (String.length str) in
+  if idx >= len then idx
+  else
+    match str.[idx] with
+    | '+' | '-' | '*' | '/' | '%' | '(' | ')' | ' ' -> idx
+    | _ -> get_next_op str (idx + 1)
+  
+
+let rec get_numbers str idx l =
+  let len = (String.length str) in
+  if idx >= len then (List.rev l)
+  else match str.[idx] with
+       | '+' | '-' | '*' | '/' | '%' | '(' | ')' | ' ' -> get_numbers str (idx + 1) l
+       | _ -> get_numbers str (get_next_op str idx) (( { value = (get_cur_nbr str idx) ; sign = 0 } )::l)
+
+
+let rec print_list_bigint = function
+  | [] -> ()
+  | h::t ->
+     begin
+       print_string "Values: ";
+       List.iter print_char h.value;
+       print_endline "";
+       print_string "Sign: ";
+       print_int h.sign;
+       print_endline "";
+       print_endline "----------";
+       print_list_bigint t;
+     end
+     
 
 let () = print_int  (eval_expr (Mul ( (Val 2), (Sum ( (Val 1), (Val 1) ) ) ) ) );;
   print_endline "";;
-let () = print_endline (compile_expr (Mul ( (Val 2), (Sum ( (Val 1), (Val 1) ) ) ) ));;
+  let () = List.iter print_char (getops "(1*2 +(3/3) - (6))" 0 []);;
+    print_endline "";;
+    let () = print_list_bigint (get_numbers "(1 * 2765 +(30/43) - (690))" 0 []);;
