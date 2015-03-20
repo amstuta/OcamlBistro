063c72bd4b44faa5df0080f859af0611133c1884
amstut_a <arthur.amstutz@gmail.com>
2015-03-20 16:39:52 +0100

    add compile_expr

--- eval_expr.ml
+++ eval_expr.ml
@@ -14,4 +14,15 @@ let rec eval_expr = function
   | Mod(expr1, expr2) -> (eval_expr expr1) mod (eval_expr expr2)
   | Val(value) -> value;;
 
- print_int  (eval_expr (Mul ( (Val 2), (Sum ( (Val 1), (Val 1) ) ) ) ) );;
+let rec compile_expr = function
+  | Sum(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "+"; (compile_expr expr2); ")"]
+  | Sub(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "-"; (compile_expr expr2); ")"]
+  | Mul(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "*"; (compile_expr expr2); ")"]
+  | Div(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "/"; (compile_expr expr2); ")"]
+  | Mod(expr1, expr2) -> String.concat "" ["("; (compile_expr expr1); "%"; (compile_expr expr2); ")"]
+  | Val(value) -> string_of_int value
+
+
+let () = print_int  (eval_expr (Mul ( (Val 2), (Sum ( (Val 1), (Val 1) ) ) ) ) );;
+  print_endline "";;
+let () = print_endline (compile_expr (Mul ( (Val 2), (Sum ( (Val 1), (Val 1) ) ) ) ));;
