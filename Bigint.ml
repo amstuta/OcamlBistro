99e620c7aa725536ef06c21b76766b8ab981e82f
amstut_a <arthur.amstutz@gmail.com>
2015-03-20 18:52:31 +0100

    addition infinie sur deux bigints unsigned

--- Bigint.ml
+++ Bigint.ml
@@ -48,31 +48,80 @@ let rec list_of_string str =
 let bigint_of_string str = ({value = (list_of_string str) ; sign = 0})
 
 
-let a = 'e'::'a'::[];;
-let b = { value = a; sign = 0 };;
-  print_list b.value;;
-    print_endline "";;
+
+let reverse_str s =
+  let rec rev_in i =
+    if i >= String.length s then ""
+    else (rev_in (i+1))^(String.make 1 s.[i])
+  in rev_in 0
+
+let rec add_zeros str nb idx nstr =
+  let len = String.length str in
+  if idx >= len then ("0"^nstr^str)
+  else match nb with
+    | 0 -> (nstr^str)
+    | _ -> add_zeros str (nb - 1) (idx + 1) (nstr^"0")
+
+		     
+let add b1 b2 =
+  let rec sub_add l1 l2 res ret =
+    match l1 with
+    | []   ->
+       begin
+	 if ret = 1 then '1'::res
+	 else res
+       end
+    | h::t ->
+       begin
+	 let v1 = (Char.code h) in
+	 let v2 = (Char.code (List.hd l2)) - 48 in
+	 if (v1 + v2 + ret) > 57 then
+	   sub_add t (List.tl l2) ((Char.chr (v1 + v2 + ret - 10))::res) 1
+	 else
+	   sub_add t (List.tl l2) ((Char.chr (v1 + v2 + ret))::res) 0
+       end
+  in { value = (sub_add (List.rev b1.value) (List.rev b2.value) [] 0) ;
+       sign = 0 }
+
+  
+let tests =
+  let a = 'e'::'a'::[] in
+  let b = { value = a; sign = 0 } in
+  print_list b.value;
+  print_endline "";
       
-    let c = convert_base "0123456789" "01" 10;;
-      print_list_int c;;
-	print_endline "";;
+  let c = convert_base "0123456789" "01" 10 in
+  print_list_int c;
+  print_endline "";
 
-      let d = convert_base "0123456789" "0123456789ABCDEF" 11;;
-	print_list_int d;;
-	  print_endline "";;
+  let d = convert_base "0123456789" "0123456789ABCDEF" 11 in
+  print_list_int d;
+  print_endline "";
 	    
-	  let e = convert_base "0123456789" "01234567" 11;;
-	    print_list_int e;;
-	      print_endline "";;
+  let e = convert_base "0123456789" "01234567" 11 in
+  print_list_int e;
+  print_endline "";
 
-	      let g = convert_base "0123456789" "0123456789ABCDEF" 13;;
-		print_list_int g;;
-		  print_endline "";;
+  let g = convert_base "0123456789" "0123456789ABCDEF" 13 in
+  print_list_int g;
+  print_endline "";
 		
-		  let f = bigint_of_string "12345";;
-		    print_string "Values: ";;
-		      print_list f.value;;
-			print_endline "";;
-			  print_string "Sign: ";;
-			    print_int f.sign;;
-			      print_endline "";;
+  let f = bigint_of_string "12345" in
+  print_string "Values: ";
+  print_list f.value;
+  print_endline "";
+  print_string "Sign: ";
+  print_int f.sign;
+  print_endline "";
+
+  (* Reverse les deux strings et les met a la meme longueur *)
+  let tmp = reverse_str (add_zeros "3" ((String.length "435") - (String.length "3")) 0 "") in
+  print_endline tmp;
+  print_endline (reverse_str "435");
+
+  (* Addition de deux bigints *)
+  let b1 = { value = ('1'::'2'::'3'::[]) ; sign = 0 } in
+  let b2 = { value = ('9'::'3'::'8'::[]) ; sign = 0} in
+  let big = add b1 b2 in
+  List.iter print_char big.value;
+  print_endline "";
